d6a5bc127cb548675169b60f2291a2b7837dc71a
Jing <763118287@qq.com>
2018-02-08 18:33:51 +0800

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -3,7 +3,7 @@ public:
 	vector<int> twoSum(vector<int>& nums, int target) {
 		for(int i = 0; i < nums.capacity(); ++i)		        
 		for(int j = 0; j < nums.capacity(); ++j){
-			if (nums[i] + nums[j] == target){
+			if (nums[i] + nums[j] == target && i != j){
 				vector<int> result;
 				result.clear();
 				result.push_back(i);
